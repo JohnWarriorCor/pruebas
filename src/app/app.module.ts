5e3c0b7b6cfb51c87af7aeb5ba0aea3fdd01314c
John David <guerrerocordobajohn@gmail.com>
2023-04-07 14:06:50 -0500

    Lector qr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { AppRouting } from "./app.routing";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { QRCodeModule } from 'angularx-qrcode';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { QRCodeModule } from "angularx-qrcode";
+import { ZXingScannerModule } from "@zxing/ngx-scanner";
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InicioComponent
-  ],
+  declarations: [AppComponent, InicioComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRouting,
-    QRCodeModule
+    QRCodeModule,
+    ZXingScannerModule,
+    MatDialogModule,
+    MatListModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatMenuModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { BarcodeFormat } from '@zxing/library';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent implements OnInit {
+export class InicioComponent {
+
+  titulo: String = 'Hello';
+
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo = null;
+
+  formatsEnabled: BarcodeFormat[] = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.QR_CODE,
+  ];
+
+  hasDevices: boolean;
+  hasPermission: boolean;
+
+  qrResultString: string;
+
+  torchEnabled = false;
+  torchAvailable$ = new BehaviorSubject<boolean>(false);
+  tryHarder = false;
 
   qrCodeOne: string = null;
   qrCodeTwo: string = null;
@@ -22,7 +45,47 @@ export class InicioComponent implements OnInit {
     this.qrCodeTwo = "https://gaitana.usco.edu.co/sgd/";
   }
 
-  ngOnInit() {
+  clearResult(): void {
+    this.qrResultString = null;
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
+  }
+
+  onCodeResult(resultString: string) {
+    this.qrResultString = resultString;
+    this.titulo = this.qrResultString;
+  }
+
+  onDeviceSelectChange(selected: string) {
+    const device = this.availableDevices.find(x => x.deviceId === selected);
+    this.currentDevice = device || null;
+  }
+
+  openFormatsDialog() {
+    const data = {
+      formatsEnabled: this.formatsEnabled,
+    };
+
+  }
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
+  }
+
+
+  onTorchCompatible(isCompatible: boolean): void {
+    this.torchAvailable$.next(isCompatible || false);
+  }
+
+  toggleTorch(): void {
+    this.torchEnabled = !this.torchEnabled;
+  }
+
+  toggleTryHarder(): void {
+    this.tryHarder = !this.tryHarder;
   }
 
 }
